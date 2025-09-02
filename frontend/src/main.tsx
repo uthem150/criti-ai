3df0f45d8b8305c1a6e42d46f6caf6def67e7980
kim dohun <uthem150@gmail.com>
2025-09-02 15:35:42 +0900

    chore(dev): 사이드바 개발을 위한 테스트 페이지 추가
    - 뉴스 기사 페이지를 시뮬레이션하는 새로운 DevTestPage를 생성
    - 이를 통해 크롬 확장 프로그램 환경 외부에서도 ContentScriptApp 및 하위 컴포넌트들의 HMR(Hot-reloading)과 테스트를 용이하게
    - 'npm run dev' 실행 시 이 테스트 페이지가 렌더링되도록 main.tsx를 업데이트합니다.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { DevTestPage } from './pages/DevTestPage'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// 개발 테스트용 진입점
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <DevTestPage />
+  </React.StrictMode>,
 )
